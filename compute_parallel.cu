e16eed4f2b1130256b477545293f70a492c14b1f
Craig Barber <RacgiMan@icloud.com>
2023-05-12 15:07:40 -0400

    please work pls

--- compute_parallel.cu
+++ compute_parallel.cu
@@ -7,7 +7,7 @@
 
 __global__ 
 void dumb_kernel(int size, int* matrix) {
-	printf("%d is the current thread\n", threadIdx.x);
+	//printf("%d is the current thread\n", threadIdx.x);
 	for (int i=0; i<size*10; i++) {
 		matrix[i] = i;
 		/*

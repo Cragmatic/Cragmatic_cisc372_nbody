7bec1e9e312eb41377b0dbc131e88128e1b22f4c
Craig Barber <RacgiMan@icloud.com>
2023-05-12 02:56:59 -0400

    toying around with cuda, writing a dumb kernel

--- compute.h
+++ compute.h
@@ -1,1 +1,2 @@
 void compute();
+void do_thing();
---
+++ compute_parallel.cu
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <math.h>
+#include "vector.h"
+#include "config.h"
+
+
+__global__ void dumb_kernel(int size, int** matrix) {
+	for (i=0; i<size; i++) {
+		for (j=0; j<size; j++) {
+			matrix[i][j] = threadIdx.x;
+		}
+	}
+	
+}
+
+//compute: Updates the positions and locations of the objects in the system based on gravity.
+//Parameters: None
+//Returns: None
+//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
+void compute(){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int i,j,k;
+	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	for (i=0;i<NUMENTITIES;i++)
+		accels[i]=&values[i*NUMENTITIES];
+	//first compute the pairwise accelerations.  Effect is on the first argument.
+	for (i=0;i<NUMENTITIES;i++){
+		for (j=0;j<NUMENTITIES;j++){
+			if (i==j) {
+				FILL_VECTOR(accels[i][j],0,0,0);
+			}
+			else{
+				vector3 distance;
+				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+			}
+		}
+	}
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
+		}
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]=hVel[i][k]*INTERVAL;
+		}
+	}
+	free(accels);
+	free(values);
+}
+
+
+__global__
+void sum_rows(int n, vector3* accel_sum, vector3** accels) {
+	int index = threadIdx.x;
+	int stride = blockDim.x;
+	for (int i=0; i<n; i+=stride) {
+		accel_sum[n] +=
+	}
+}
+
+
+void do_thing() {
+	cudaMallocManaged(&my_matrix, 10*10*sizeof(int));
+	int** my_matrix = (int**)malloc(sizeof(int)*10*10);
+	
+	<<<dumb_kernel>>>(10, my_matrix);
+	for (int i = 0; i < 10; i++) {
+		printf("\n")
+		for (int j = 0; j < 10; j++) {
+			printf("%d\t")
+		}
+	}
+	cudaDeviceSynchronize();
+}

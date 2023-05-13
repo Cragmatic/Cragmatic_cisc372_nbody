ae805fd6a781fc801a205adc5cfea53eac554c37
Craig Barber <RacgiMan@icloud.com>
2023-05-13 16:45:02 -0400

    how about d_hPos

--- compute_parallel.cu
+++ compute_parallel.cu
@@ -21,12 +21,12 @@ __global__ void pairwise_accel(vector3* d_accels, vector3* d_hPos, vector3* d_hV
 	}
 	else{
 		vector3 distance;
-		for (k=0;k<3;k++) distance[k]=d_hPos[i][k]-d_hPos[j][k];
+		for (k=0;k<3;k++) distance[k]=d_hPos[i][k]-d_hPos[j][k];printf("distances: at %d,%d: %f\t%f\t%f\n", i, j, distance[0],distance[1],distance[2])
 		double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
 		double magnitude=sqrt(magnitude_sq);
 		double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
 		FILL_VECTOR(d_accels[i*NUMENTITIES+j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-		printf("accels at %d, %d: %f\t%f\t%f\n", i, j, d_accels[i*NUMENTITIES+j][0],d_accels[i*NUMENTITIES+j][1],d_accels[i*NUMENTITIES+j][2]);
+		//printf("accels at %d, %d: %f\t%f\t%f\n", i, j, d_accels[i*NUMENTITIES+j][0],d_accels[i*NUMENTITIES+j][1],d_accels[i*NUMENTITIES+j][2]);
 	}
 }
 
@@ -36,7 +36,7 @@ __global__ void sum_rows_and_compute(vector3* d_accels, vector3* d_hPos, vector3
 	//Assuming we spawn enough blocks+threads to cover the whole NUMENTITIESxNUMENTITIES matrix, each thread does 1 calculation
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	
+
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 		vector3 accel_sum={0,0,0};
 		for (k=0;k<3;k++)

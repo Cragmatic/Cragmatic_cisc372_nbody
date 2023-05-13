17f81571daf3c29e2cb0b783e93c72e5bbeb63e9
Craig Barber <RacgiMan@icloud.com>
2023-05-13 15:56:14 -0400

    fixed header so function defined

--- compute_parallel.h
+++ compute_parallel.h
@@ -1,1 +1,2 @@
-void compute(vector3* d_hPos, vector3* d_hVel, dim3 dimBlock, dim3 dimGrid);
+__global__ void pairwise_accel(vector3* d_hPos, vector3* d_hVel, double* mass);
+//void compute(vector3* d_hPos, vector3* d_hVel, dim3 dimBlock, dim3 dimGrid);

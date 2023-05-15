0b7d0d33f7bbc9275576b6c2d4447ee34648b5fe
Craig Barber <RacgiMan@icloud.com>
2023-05-14 21:42:59 -0400

    why does it break when 1000 planets :(

--- compute_parallel.cu
+++ compute_parallel.cu
@@ -11,13 +11,13 @@ __global__ void pairwise_accel(vector3* d_accels, vector3* d_hPos, vector3* d_hV
 	//Assuming we spawn enough blocks+threads to cover the whole NUMENTITIESxNUMENTITIES matrix, each thread does 1 calculation
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	if (blockDim.y > 0) {
-	printf("Hello from thread coordinates %d, %d with args bidx.x: %d, bdim.x: %d, tidx.x: %d, bidx.y: %d, bdim.y: %d, tidx.y: %d, mass: %d\n", 
-	i, j, blockIdx.x, blockDim.x, threadIdx.x, blockIdx.y, blockDim.y, threadIdx.y, d_mass[j]);
-	}
 	//HELLO? WHAT THE FRICK?
 	if (i >= NUMENTITIES || j >= NUMENTITIES) {
 		return;
+	}
+	if (blockDim.y > 0) {
+	printf("Hello from thread coordinates %d, %d with args bidx.x: %d, bdim.x: %d, tidx.x: %d, bidx.y: %d, bdim.y: %d, tidx.y: %d, mass: %d\n", 
+	i, j, blockIdx.x, blockDim.x, threadIdx.x, blockIdx.y, blockDim.y, threadIdx.y, d_mass[j]);
 	}
 	if (i==j) {
 		FILL_VECTOR(d_accels[i*NUMENTITIES+j],0,0,0);

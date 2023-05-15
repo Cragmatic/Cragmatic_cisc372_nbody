853bd9a8de4e1c062ecca4d5c916f778e9aff252
Craig Barber <RacgiMan@icloud.com>
2023-05-14 22:02:51 -0400

    why does it break when 1000 planets :(

--- nbody_parallel.cu
+++ nbody_parallel.cu
@@ -118,7 +118,7 @@ int main(int argc, char **argv)
 	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
 	//now we have a system.
 	#ifdef DEBUG
-		printSystem(stdout);
+		//printSystem(stdout);
 	#endif
 
     //start block
@@ -137,11 +137,11 @@ int main(int argc, char **argv)
 	dim3 dimGrid((NUMENTITIES+dimBlock.x-1)/dimBlock.x, (NUMENTITIES+dimBlock.y-1)/dimBlock.y);
 	//printf("\n\ndimGrid: %d, %d",dimGrid.x, dimGrid.y);
     //end block
-	//cudaDeviceSynchronize();
-	//print_from_kernel<<<1,1>>>(d_accels, d_hPos, d_hVel, dev_mass);
+	cudaDeviceSynchronize();
+	print_from_kernel<<<1,1>>>(d_accels, d_hPos, d_hVel, dev_mass);
 
 	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
-		compute(d_accels, d_hPos, d_hVel, dimBlock, dimGrid, dev_mass); //Altered
+		//compute(d_accels, d_hPos, d_hVel, dimBlock, dimGrid, dev_mass); //Altered
 		//compute<<<1, 1>>>(d_accels, d_hPos, d_hVel, dimBlock, dimGrid, dev_mass);
 		//printf("I am about to call pairwise. wow!!!");
 		//pairwise_accel<<<dimGrid, dimBlock, dimBlock.x*dimBlock.y*sizeof(vector3)>>>(d_hPos, d_hVel, mass);
@@ -163,7 +163,7 @@ int main(int argc, char **argv)
 
 	clock_t t1=clock()-t0;
 #ifdef DEBUG
-	printSystem(stdout);
+	//printSystem(stdout);
 #endif
 	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
 

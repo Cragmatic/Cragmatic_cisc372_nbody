58344436288078e2af69d385d0aca734245ecf34
Craig Barber <RacgiMan@icloud.com>
2023-05-15 00:26:15 -0400

    same story as before; Fixing syntax errors in compute_parallel.cu

--- compute_parallel.cu
+++ compute_parallel.cu
@@ -4,23 +4,11 @@
 #include "config.h"
 #include <stdio.h>
 
-//compute: Updates the positions and locations of the objects in the system based on gravity.
-//Parameters: None
-//Returns: None
-//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute(){
-	dim3 dimBlock(16, 16);
-	dim3 dimGrid((NUMENTITIES+dimBlock.x-1)/dimBlock.x, (NUMENTITIES+dimBlock.y-1)/dimBlock.y);
-	compute_pairwise<<<dimGrid, dimBlock>>>(d_accels, d_hPos, d_mass);
-	dim3 dimBlock(256);
-	dim3 dimGrid((NUMENTITIES+dimBlock.x-1)/dimBlock.x);
-	update_bodies<<<dimGrid, dimBlock>>>(d_accels, d_hVel, d_mass)
-}
-
 __global__ void compute_pairwise(vector3* accels, vector3* hPos, double* mass) {
 	//Assuming this is going to be called with enough thread blocks + threads that each is doing 1 calculation.
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
+	int k;
 	if (i >= NUMENTITIES || j >= NUMENTITIES) {
 		return; //avoids touching memory not in use (extending past the # of entities we have)
 	}
@@ -42,6 +30,7 @@ __global__ void compute_pairwise(vector3* accels, vector3* hPos, double* mass) {
 __global__ void update_bodies(vector3* accels, vector3* hVel, vector3* hPos) {
 	//Assuming that each call to update_bodies has enough blocks/threads that each is summing one column.
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j, k;
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 	vector3 accel_sum={0,0,0};
 	for (j=0;j<NUMENTITIES;j++){
@@ -54,4 +43,17 @@ __global__ void update_bodies(vector3* accels, vector3* hVel, vector3* hPos) {
 		hVel[i][k]+=accel_sum[k]*INTERVAL;
 		hPos[i][k]=hVel[i][k]*INTERVAL;
 	}
+}
+
+//compute: Updates the positions and locations of the objects in the system based on gravity.
+//Parameters: None
+//Returns: None
+//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
+void compute(vector3* d_accels, vector3* d_hPos, vector3* d_hVel, double* d_mass){
+	dim3 dimBlock(16, 16);
+	dim3 dimGrid((NUMENTITIES+dimBlock.x-1)/dimBlock.x, (NUMENTITIES+dimBlock.y-1)/dimBlock.y);
+	compute_pairwise<<<dimGrid, dimBlock>>>(d_accels, d_hPos, d_mass);
+	dim3 dimBlock2(256);
+	dim3 dimGrid2((NUMENTITIES+dimBlock.x-1)/dimBlock.x);
+	update_bodies<<<dimGrid2, dimBlock2>>>(d_accels, d_hVel, d_mass);
 }
